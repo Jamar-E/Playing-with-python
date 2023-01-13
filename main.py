24b764d54730afb14a164e13404f049822e696bb
Jamar-E <jamark@hotmail.co.uk>
2023-01-13 17:32:37 +0000

    simple Madlib

---
+++ main.py
@@ -0,0 +1,9 @@
+color = input("enter a color: ")
+Plural_noun = input("enter a plural noun:")
+Celebrity = input("enter a Celebrity name: ")
+
+print("-----------------<^-^>-----------------")
+print("Roses are " + color)
+print(Plural_noun + " are blue ")
+print("I love "+ Celebrity )
+print("how about you?")
