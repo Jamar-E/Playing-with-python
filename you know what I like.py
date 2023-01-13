2af966b7926234f382dedff3374b6a6ba87344a3
Jamar-E <jamark@hotmail.co.uk>
2023-01-13 17:58:53 +0000

    you know what I like?
    Using user input to call from a list.
    Then accessing and displaying specifics from the user input

---
+++ you know what I like.py
@@ -0,0 +1,11 @@
+
+people = ["dude1", "dude2", "dude3", "dude4"]
+like = input("what's your favorite food? ")
+
+print(people[0] + " and " + people[1])
+print("went to a bar")
+print(people[0] + " then said")
+print('\"you know what I like?. . .\"')
+print(like)
+
+print("mmmmmmm " + like[0] + like[1] + like[1] + like[1] + like[1:])
