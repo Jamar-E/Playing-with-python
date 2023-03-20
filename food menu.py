529977545b1eea2781d4a3819c7a36adb27ab43d
Jamar-E <jamark@hotmail.co.uk>
2023-03-20 10:25:22 +0000

    A simple yet judgie  food

---
+++ food menu.py
@@ -0,0 +1,27 @@
+age = "hi"
+
+veg = ["apples", "celary", "i don't know leaves maby?"]
+vagitarian = ["apples", "celary", "i don't know leaves maby?"]
+vegan =  ["Hemp","flax" , "chia seeds"]
+nope = ["burgur", "bacon", "eggs"]
+
+print("do you have any dietary requirements?")
+print("veg (vagitarian), vegan, Nope")
+
+Diet_Req = input().lower()
+
+if Diet_Req == "vegan":
+  print("Take va look at these options")
+  print(vegan)
+
+if Diet_Req == "vagitarian":
+  print("ohhhh so you're difficault. Here are your options:")
+  print(veg)
+
+if Diet_Req == "veg":
+  print("ohhhh so you're difficault. here are your options")
+  print(veg)
+
+if Diet_Req == "nope":
+  print("ahh you're one of those Here are your options:")
+  print(nope)
